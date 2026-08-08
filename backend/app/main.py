4208508bd7278756d6ea12a50ece51a02cf9e26c
jonbi171 <jonte.billger@gmail.com>
2026-08-09 00:30:37 +0200

    Added first small test KPI route

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from typing import Any
 import backend.database.models as dbModels
 from backend.database.config import boot
 from fastapi import Depends, FastAPI
+from fastapi.responses import PlainTextResponse
 from sqlalchemy import inspect, select
 from sqlalchemy.orm import Session
 
@@ -38,3 +39,24 @@ def list_product_suppliers(
         model_to_dict(product_supplier)
         for product_supplier in product_suppliers
     ]
+
+@app.get("/product-suppliers-order-cost", response_class=PlainTextResponse)
+def list_product_suppliers_by_order_cost(
+    session: Session = Depends(get_session),
+) -> str:
+    statement = select(
+        dbModels.ProductSuppliers.minimum_order_quantity,
+        dbModels.ProductSuppliers.unit_cost_sek,
+    )
+
+    rows = session.execute(statement).mappings().all()
+
+    costs = "\n".join(
+        str(row['minimum_order_quantity'] * row['unit_cost_sek'])
+        for row in rows
+    )
+
+    return (
+        "The product suppliers have the following minimum order costs:\n"
+        f"{costs}"
+    )
